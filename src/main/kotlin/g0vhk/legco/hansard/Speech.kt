345234976a37cfe830b75d5edb7018f42fe6779a
howa <howa.wong@gmail.com>
2018-01-04 02:36:20 +0800

    Strip number in title

--- src/main/kotlin/g0vhk/legco/hansard/Speech.kt
+++ src/main/kotlin/g0vhk/legco/hansard/Speech.kt
@@ -1,10 +1,18 @@
 package g0vhk.legco.hansard
 
+import java.util.regex.Pattern
+
 class Speech {
     val title: String
     val content: String
     constructor (title: String, content: String){
-        this.title = title
+        val p = Pattern.compile("^(\\d+. +)(.*)")
+        val m = p.matcher(title)
+        if (m.find()) {
+            this.title = m.group(2)
+        } else {
+            this.title = title
+        }
         this.content = content
     }
 }
